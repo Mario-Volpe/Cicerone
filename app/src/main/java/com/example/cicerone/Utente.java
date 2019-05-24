532cefab7248303f20cd80299e934e6774f61a6e
TheBestFox <marioh@live.it>
2019-05-24 19:15:01 +0200

    Prima versione

---
+++ app/src/main/java/com/example/cicerone/Utente.java
@@ -0,0 +1,63 @@
+package com.example.cicerone;
+
+import android.provider.ContactsContract;
+
+import java.util.UUID;
+
+public class Utente {
+    private static final Utente ourInstance = new Utente();
+
+    public static Utente getInstance() {
+        return ourInstance;
+    }
+
+    private UUID id = UUID.randomUUID();
+
+    private String password;
+    private String cognome;
+    private String nome;
+    private ContactsContract.CommonDataKinds.Email email;
+
+    public ContactsContract.CommonDataKinds.Email getEmail() {
+        return email;
+    }
+
+    public void setEmail(ContactsContract.CommonDataKinds.Email email) {
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    private Utente() {
+    }
+}
