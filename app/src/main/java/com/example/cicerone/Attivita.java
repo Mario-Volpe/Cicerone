9a429b17ab3e6a92a261d18bcf10b746d167308a
TheBestFox <marioh@live.it>
2019-05-29 14:40:15 +0200

    Seconda versione

---
+++ app/src/main/java/com/example/cicerone/Attivita.java
@@ -0,0 +1,14 @@
+package com.example.cicerone;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Attivita {
+    Utente Cicerone;
+    Calendar data;
+    String descrizioneItinerario;
+    String lingua;
+    Integer maxPartecipanti;
+
+
+}
