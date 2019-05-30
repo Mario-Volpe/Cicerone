9240c923aa41ffd07b81f1b6cf477cd56ecbf8de
Andrea Montemurro <andrea.montemurro97@gmail.com>
2019-05-30 22:14:50 +0200

    Creato leyout main Activity e creato bottone che chiama activity login dalla main act.

--- app/src/main/java/com/example/cicerone/MainActivity.java
+++ app/src/main/java/com/example/cicerone/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.cicerone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.example.cicerone.ui.login.LoginActivity;
+import java.nio.file.Files;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonLogin;
+    private Button buttonRegistration;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //bottone che passa all'activity login
+        buttonLogin = (Button) findViewById(R.id.button_login);
+        //bottone che passa all'activity registrazione
+        buttonRegistration = (Button) findViewById(R.id.button_registrazione);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityLogin();
+            }
+        });
+
+        buttonRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityRegistration();
+            }
+        });
+    }
+
+    public void openActivityLogin()
+    {
+        Intent Loginintent = new Intent(this, LoginActivity.class );
+        startActivity(Loginintent);
+    }
+
+    //bisogna creare registration activity
+    public void openActivityRegistration()
+    {
+        Intent RegistrationIntent = new Intent(this, RegistrationActivity.class);
     }
 }
